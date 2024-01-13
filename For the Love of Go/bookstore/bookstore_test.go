8510b9a9bcf47467a4eeb44a0e174870d6db9172
aliraza7926 <aliraza7926@gmail.com>
2024-01-13 22:54:17 +0330

    write a simple buy function and it's test

--- For the Love of Go/bookstore/bookstore_test.go
+++ For the Love of Go/bookstore/bookstore_test.go
@@ -13,3 +13,21 @@ func TestBook(t *testing.T) {
 		Copies: 3,
 	}
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "For the Love of Go",
+		Author: "John Arundel",
+		Copies: 3,
+	}
+
+	want := 2
+	result := bookstore.Buy(b)
+	got := result.Copies
+
+	if want != got {
+		t.Errorf("want %d copies got %d", want, got)
+	}
+
+}
