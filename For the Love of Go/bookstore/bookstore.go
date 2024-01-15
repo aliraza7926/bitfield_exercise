350012d809e72c1f498ba5460113170ab62a1602
aliraza7926 <aliraza7926@gmail.com>
2024-01-15 19:36:25 +0330

    create a getbook function and it's test

--- For the Love of Go/bookstore/bookstore.go
+++ For the Love of Go/bookstore/bookstore.go
@@ -1,12 +1,15 @@
 package bookstore
 
-import "errors"
+import (
+	"errors"
+)
 
 // Book represents information about a book.
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+	ID     int
 }
 
 func Buy(b Book) (Book, error) {
@@ -19,4 +22,13 @@ func Buy(b Book) (Book, error) {
 
 func GetAllBooks(catalog []Book) []Book {
 	return catalog
+}
+
+func GetBook(catalog []Book, ID int) Book {
+	for _, book := range catalog {
+		if book.ID == ID {
+			return book
+		}
+	}
+	return Book{}
 }
